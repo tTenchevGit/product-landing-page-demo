ff7e3ccd030854dcf906cd9007facc92bbfe9c92
Toni Tenchev <tonitenchev93@gmail.com>
2024-06-05 09:42:02 +0100

    practice

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p> &copy; Copyright 2021</p>
+    </footer>
+  );
+};
+
+export default Footer;
