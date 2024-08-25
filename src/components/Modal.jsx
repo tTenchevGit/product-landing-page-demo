734a59035e55356ac16b0ce4e29e2d6e8dabdc5a
Toni Tenchev <tonitenchev93@gmail.com>
2024-08-25 12:55:17 +0100

    dark mode updated

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,7 +14,7 @@ const Modal = ({ show, onClose }) => {
                 <h2 style={{ color: 'white' }}>Amazing Things Happening</h2>
                 <img width={250} src="https://m.media-amazon.com/images/I/61A+fuSj3FL._AC_SL1500_.jpg" alt="massage" />
                 <p className={styles.description}>
-                    Massage is a relaxing and therapeutic treatment that can be performed on the body, face, and hands. It can be performed on the body, face, and hands. It can be performed on the body, face, and hands. It can be performed on the body, face, and hands. It can be performed on the body, face, and hands. It can be performed on the body, face, and hands.
+                    Massage is a relaxing and therapeutic 
                 </p>
                 
             </div>
