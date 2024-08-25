31b9e3bf60aeea5d7791ff319072695a53ef3575
Toni Tenchev <tonitenchev93@gmail.com>
2024-08-25 12:56:36 +0100

    modal update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,7 +14,7 @@ const Modal = ({ show, onClose }) => {
                 <h2 style={{ color: 'white' }}>Amazing Things Happening</h2>
                 <img width={250} src="https://m.media-amazon.com/images/I/61A+fuSj3FL._AC_SL1500_.jpg" alt="massage" />
                 <p className={styles.description}>
-                    Massage is a relaxing and therapeutic 
+                    Massage is a relaxing and therapeutic experience. It is a great way to relieve stress and tension, and can help improve circulation and flexibility. Massage can also help reduce pain and inflammation, and promote healing in the body. Whether you are looking for a relaxing massage or a therapeutic treatment, we have a variety of options to choose from. Our experienced massage therapists will work with you to create a customized treatment plan that meets your individual needs. Book your appointment today and experience the amazing benefits of massage therapy!
                 </p>
                 
             </div>
